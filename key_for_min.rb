57a758426adfbda69ae11c31c853bb70026ae942
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 04:29:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    name_hash.collect each do | name, value |
+    name_hash.collect do | name, value |
+      
 
   
 end
