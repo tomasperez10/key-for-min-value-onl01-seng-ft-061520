1c081f6911f23bac9268041f730a66e3611d6040
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 04:37:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = 0
+  lowest_value = 0
+  lowest_key = 
   
   if name_hash == {}
     return nil
